b364e8cffc7842e7d10b514ce0b09f94b502e14f
Sergey Zobin <zobin.sergey@gmail.com>
2019-07-29 14:40:07 +0300

    fix3

--- ours/wsgi.py
+++ ours/wsgi.py
@@ -7,9 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
+
+BASE_DIR = "/home"
+
+sys.path.append(os.path.join(BASE_DIR, 'ours'))
+sys.path.append(os.path.join(BASE_DIR, 'ours', 'ours'))
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ours.settings')
 
